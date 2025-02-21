129b5dcb4451bcea622d1f81a6ef7eb7028ade6f
mukulkumawat2412 <mukulkumawat2412@gmail.com>
2025-02-21 21:00:32 +0530

    Fix package.json issue

--- api/server.js
+++ api/server.js
@@ -22,7 +22,7 @@ const frontendRouter = require("./routes/frontend")
 
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://127.0.0.1:27017/merndb", {
+mongoose.connect(" mongodb://127.0.0.1:27017/mydatabase", {
   
   });
   
